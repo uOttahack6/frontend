ad7ecf2e8d293c74f5fd70ed825a17be35a8af28
aimlessjellyfish <chilliestmochi@gmail.com>
2024-03-02 09:31:35 -0500

    Fixed some text alignment

--- src/Pages/Leaderboards.js
+++ src/Pages/Leaderboards.js
@@ -3,8 +3,29 @@ import React from 'react';
 function Leaderboards() {
   return (
     <div>
-      <h2>Leadership boards</h2>
-      <p>Contact us page content.</p>
+      <h2 class="my-5 mx-4">Leadership boards</h2>
+      <div class="row">
+        <div class="col-sm-5 mx-4">
+          <div class="card">
+            <div class="card-header">
+              Compare with Friends
+            </div>
+            <div class="card-body">
+              list of names by points
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5">
+          <div class="card">
+            <div class="card-header">
+              Compare with Neighbours
+            </div>
+            <div class="card-body">
+              list of names by points
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
