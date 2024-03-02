eb45ffee129ad73dbac724dc005dbbcda53b84fc
aimlessjellyfish <chilliestmochi@gmail.com>
2024-03-02 09:00:59 -0500

    Updated Home and Navigation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Home from './Pages/Home';
-import About from './Pages/About';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Container } from 'reactstrap';
 import LoginPage from './Pages/Login';
 
-import Contact from './Pages/Contact';
 import Navbar from './Navbar';
+import Leaderboards from './Pages/Leaderboards';
+import Upload from './Pages/Upload';
+import Tips from './Pages/Tips';
 
 function App() {
   return (
@@ -17,18 +18,13 @@ function App() {
         {/* Authenticated */}
         <Authenticated />
         <Navbar />
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
 
         {/* Content */}
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/leaderboards" element={<Leaderboards />} />
+          <Route path="/tips" element={<Tips />} />
         </Routes>
       </div>
     </Router>
