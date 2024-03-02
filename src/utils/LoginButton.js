ae1cc9bb5d92b1d2a88ecd5668f4c44d96eb06ef
ccm32004 <chingching.ma32004@gmail.com>
2024-03-02 08:37:38 -0500

    added login and logout

---
+++ src/utils/LoginButton.js
@@ -0,0 +1,18 @@
+// src/utils/LoginButton.js
+import React from 'react';
+import { Button } from 'reactstrap'; // Import Button from reactstrap
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleLogin = () => {
+    loginWithRedirect();
+  };
+
+  return (
+    <Button onClick={handleLogin}>Login</Button>
+  );
+};
+
+export default LoginButton;
