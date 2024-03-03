5be2c0da83ac75e938762b59460cd44bde6c26a5
aimlessjellyfish <chilliestmochi@gmail.com>
2024-03-02 20:34:10 -0500

    Trying to fix alignment

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
-//import "bootstrap/dist/js/bootstrap.bundle.min";
-//import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
   return (
     <div>
-      <h5 class="mx-4 my-5">
+      <h5 class="mx-5 my-5">
         Sustainability Points:
       </h5>
       <div class="row">
-        <div class="col-sm-5 mx-4">
+        <div class="col-5 mx-5">
           <div class="card">
             <div class="card-header">
               Sustainability Task of the Day
@@ -19,12 +19,12 @@ function Home() {
             </div>
           </div>
         </div>
-        <div class="col-sm-5">
+        <div class="col-5 mx-5">
           <div class="card">
             <div class="card-header">
               Long Term Quests
             </div>
-            <ul>
+            <ul class="card-body">
               <li class="list-group-item"> Task 1</li>
               <li class="list-group-item"> Task 2</li>
               <li class="list-group-item"> Task 3</li>
@@ -32,11 +32,11 @@ function Home() {
           </div>
         </div>
       </div>
-      <div class="card col-sm-5 mx-4">
+      <div class="card col-5 mx-5 my-5">
         <div class="card-header">
           Daily Quest
         </div>
-        <ul>
+        <ul class="card-body">
             <li class="list-group-item"> Task 1</li>
             <li class="list-group-item"> Task 2</li>
             <li class="list-group-item"> Task 3</li>
