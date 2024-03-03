12e2ef1c00525b6506eecbf6df89506f479728bf
ccm32004 <chingching.ma32004@gmail.com>
2024-03-02 22:05:45 -0500

    request total score api endpoint

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,45 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
+  const [totalScore, setTotalScore] = useState(null);
+
+  useEffect(() => {
+    const fetchTotalScore = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/users/1/total-score');
+        const data = response.data;
+        setTotalScore(data.totalScore);
+      } catch (error) {
+        console.error('Error fetching total score:', error.message);
+      }
+    };
+
+    fetchTotalScore();
+  }, []); // Empty dependency array to run the effect only once
+
   return (
     <div>
-      <h5 class="mx-5 my-5">
-        Sustainability Points:
+      <h5 className="mx-5 my-5">
+        Sustainability Points: {totalScore !== null ? totalScore : 'Loading...'}
       </h5>
-      <div class="row">
-        <div class="col-5 mx-5">
-          <div class="card">
-            <div class="card-header">
+      <div className="row">
+        <div className="col-5 mx-5">
+          <div className="card">
+            <div className="card-header">
               Sustainability Task of the Day
             </div>
-            <div class="card-body">
+            <div className="card-body">
               Words
             </div>
           </div>
         </div>
-        <div class="col-5 mx-5">
-          <div class="card">
-            <div class="card-header">
+        <div className="col-5 mx-5">
+          <div className="card">
+            <div className="card-header">
               Long Term Quests
             </div>
-            <ul class="card-body">
-              <li class="list-group-item"> Task 1</li>
-              <li class="list-group-item"> Task 2</li>
-              <li class="list-group-item"> Task 3</li>
+            <ul className="card-body">
+              <li className="list-group-item"> Task 1</li>
+              <li className="list-group-item"> Task 2</li>
+              <li className="list-group-item"> Task 3</li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="card col-5 mx-5 my-5">
-        <div class="card-header">
+      <div className="card col-5 mx-5 my-5">
+        <div className="card-header">
           Daily Quest
         </div>
-        <ul class="card-body">
-            <li class="list-group-item"> Task 1</li>
-            <li class="list-group-item"> Task 2</li>
-            <li class="list-group-item"> Task 3</li>
+        <ul className="card-body">
+          <li className="list-group-item"> Task 1</li>
+          <li className="list-group-item"> Task 2</li>
+          <li className="list-group-item"> Task 3</li>
         </ul>
       </div>
     </div>
@@ -47,3 +64,4 @@ function Home() {
 }
 
 export default Home;
+
