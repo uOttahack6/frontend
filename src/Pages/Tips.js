2609c3f49d23cd07ff318b352506495ef34657a7
Ken Chan <echan123@uottawa.ca>
2024-03-03 09:56:08 -0500

    Add Tips by aimlessjellyfish

--- src/Pages/Tips.js
+++ src/Pages/Tips.js
@@ -1,11 +1,128 @@
 import React from 'react';
+import '../Tips.css';
 
-function Tips(){
-    return(
+function Tips() {
+    return (
         <div>
-            <h2>
-                Tips
+            <h2 class="mx-5 my-5">
+                Want to be more sustainable? Here's how!
             </h2>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Reduce, Reuse, Recycle
+                                </h5>
+                                <p class="card-text">
+                                    You can try and reduce the amount of waste created by reusing or choosing reusable options, and recycling paper, glass, metal, electronics, and plastic in appropriate places.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Energy Conservation
+                                </h5>
+                                <p class="card-text">
+                                    You can turn off lights and appliances when not in use and use energy efficient appliances. This helps save on energy bills too.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Water Conservation
+                                </h5>
+                                <p class="card-text">
+                                    You can reduce your water consumption by making sure faucets are completely closed when not in use, fixing leaks, taking shorter showers, and installing water-effient fixtures such as toilets and showerheads.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Sustainable Transportation
+                                </h5>
+                                <p class="card-text">
+                                    Choosing to walk, ride a bike, use public transportation, or carpooling helps reduce greenhouse gas emissions. Using electric and hybrid vehicles are also viable.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Sustainable Food
+                                </h5>
+                                <p class="card-text">
+                                    Reducing meat consumption, buying local and organic foods, and minimizing food waste helps greatly with environmental sustainability.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Reduce Single-Use Plastics
+                                </h5>
+                                <p class="card-text">
+                                    Instead, use reusable bags, bottles, containers, and other items which can be reused. Avoid using or buying products with excess plastic.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Plant Trees and Gardens
+                                </h5>
+                                <p class="card-text">
+                                    Trees and plants play a vital role in the environment and reducing excess greenhouse gases. Make sure to do research on plants native to your location to support the local ecosystem and avoid the spread of invasive species.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Sustainable Tourism
+                                </h5>
+                                <p class="card-text">
+                                    Support local communities when travelling, minimize environmental impact when travelling, such as respecting local culture, wildlife, and environments, doing activities which have low-impact on the environment, and managing your waste responsibly.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Education and Policies
+                                </h5>
+                                <p class="card-text">
+                                    Educate yourself and pay attention to policies and regulations about environmental issues and sustainability practices. Advocate and help take action for and against policies and initiatives that have impact.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
