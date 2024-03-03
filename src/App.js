b0cc17700b939ef6bd1cdfc316e7e33a8ec370b9
Ken Chan <echan123@uottawa.ca>
2024-03-03 04:05:23 -0500

    global msgs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Home from './Pages/Home';
@@ -11,8 +11,19 @@ import Leaderboards from './Pages/Leaderboards';
 import Upload from './Pages/Accomplishments';
 import Tips from './Pages/Tips';
 import Callback from './Pages/Callback';
+import { useState } from 'react';
+import messaging from './utils/Messaging';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [connected, setConnected] = useState(false);
+  useEffect(() => {
+    const handleMessage = (message) => {
+      setMessages(JSON.parse(message.payloadString));
+    };
+
+    messaging.register(handleMessage);
+  }, []); // Empty dependency array ensures the effect runs only once
   return (
     <Router>
       <div>
@@ -22,9 +33,9 @@ function App() {
 
         {/* Content */}
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/leaderboards" element={<Leaderboards />} />
+          <Route exact path="/" element={<Home messages={messages} setMessages={setMessages} connected={connected} setConnected={setConnected} />} />
+          <Route path="/upload" element={<Upload messages={messages} />} />
+          <Route path="/leaderboards" element={<Leaderboards messages={messages} />} />
           <Route path="/tips" element={<Tips />} />
           <Route path="/callback" element={<Callback />} />
         </Routes>
