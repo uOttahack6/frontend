5493e622fe6399211c0115ed15a42ebf2b821a8d
aimlessjellyfish <chilliestmochi@gmail.com>
2024-03-02 21:55:58 -0500

    Changed colors

--- src/Pages/Tips.js
+++ src/Pages/Tips.js
@@ -3,9 +3,13 @@ import React from 'react';
 function Tips(){
     return(
         <div>
-            <h2>
-                Tips
+            <h2 class="mx-5 my-5">
+                Want to be more sustainable?
+                Here's how!
             </h2>
+            <div class="mx-5">
+                Text goes here
+            </div>
         </div>
     );
 }
