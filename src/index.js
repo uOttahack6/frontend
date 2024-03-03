04314fd6b9b99b60ae624e187f011ebe1452a5d1
Ken Chan <echan123@uottawa.ca>
2024-03-02 21:45:11 -0500

    remove bootstrap.css

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import './index.css';
 import { AuthProvider } from './utils/AuthProvider';
 import reportWebVitals from './reportWebVitals';
 import "./Navbar.css"
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
