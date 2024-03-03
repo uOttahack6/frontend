a7c86f5b207367c7b98068e6a56f91add2a48539
maiahg <122498165+maiahg@users.noreply.github.com>
2024-03-02 20:29:32 -0500

    Added Login Page, direct user to Login Page if not authenticated

--- src/App.js
+++ src/App.js
@@ -5,53 +5,41 @@ import Home from './Pages/Home';
 import About from './Pages/About';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Container } from 'reactstrap';
-import LoginButton from './utils/LoginButton';
-import LogoutButton from './utils/LogoutButton';
+import LoginPage from './Pages/Login';
 
-const App = () => {
-  const { isAuthenticated } = useAuth0();
 
+function App() {
   return (
     <Router>
-      <div id="app" className="d-flex flex-column h-100">
-        {/* Navbar */}
-        <nav>
-          <ul>
-
-            {isAuthenticated && (
-              <>
-                <li>
-                  <Link to="/home">Home</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-                <li>
-                  <LogoutButton />
-                </li>
-              </>
-            )}
-
-          </ul>
-
-          {/* Conditionally render login button */}
-          {!isAuthenticated && (
-
-            <LoginButton />
-
-          )}
-        </nav>
+      <div>
+        {/* Navigation */}
+        <Authenticated />
 
         {/* Content */}
-        <Container className="flex-grow-1 mt-5">
-          <Routes>
-            <Route exact path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </Container>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
     </Router>
   );
-};
+}
+
+function Authenticated() {
+  const {isAuthenticated} = useAuth0();
+
+  if (!isAuthenticated) {
+    return <LoginPage />;
+  }
+
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </nav>
+  );
+}
 
 export default App;
